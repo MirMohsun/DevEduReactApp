2fcfbc0db0c8567db270b040710e60d7db412e0d
Mirmohsun <mma@dbbsoftware.com>
2019-11-30 15:56:23 +0400

    Added navigation menu and created some parts of project

--- Turtle/NavTurtle.js
+++ Turtle/NavTurtle.js
@@ -1,88 +1,76 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { createAppContainer } from 'react-navigation';
+import { View, Text, TouchableOpacity, Button } from 'react-native';
+import React, { Component } from 'react'
 import { createStackNavigator } from 'react-navigation-stack';
-import {createBottomTabNavigator } from 'react-navigation-tabs';
-import {Turtle} from './MainTurtle' 
+import { createAppContainer } from 'react-navigation';
+import { stylesForTodo, stylesForNav } from './StyleTurtle'
+import CalculatorClass from '../Calculator/calculator'
 
-
-class HomeScreen extends React.Component {
+class HomeScreen extends Component {
   static navigationOptions = {
-    title: 'Hello my home',
-    }; 
+    title: 'DevEducation',
+    headerStyle: { backgroundColor: 'red' },
+    headerTitleStyle: { color: 'black' },
+  };
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details', {
-            itemId: 86,
-            otherParam: 'anything you want here',
-          })
-          }
-        />
-
+      <View style={stylesForNav.container}>
+        <TouchableOpacity style={stylesForNav.btnStl} onPress={() => this.props.navigation.navigate('ToDolist', {
+          itemId: 86,
+          otherParam: 'anything you want here',
+        })}>
+          <Text style={[stylesForNav.btntxt]}>Go To Todo List</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={stylesForNav.btnStl} onPress={() => this.props.navigation.navigate('ToDolist', {
+          itemId: 86,
+          otherParam: 'anything you want here',
+        })}>
+          <Text style={[stylesForNav.btntxt]}>Go To some screen</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={stylesForNav.btnStl} onPress={() => this.props.navigation.navigate('Calculator')}>
+          <Text style={[stylesForNav.btntxt]}>Go To Calculator</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-class DetailsScreen extends React.Component {
-  static navigationOptions = { 
-    headerRight: () => <Text>Bye</Text>, 
-    headerTitle: () => ( <Button onPress={() => alert('This is a button!')} 
-    title="Info" color="black" /> ),
-     };
+export default class TodoList extends Component {
+  static navigationOptions = {
+    headerRight: () => <Text>Bye</Text>,
+    headerTitle: () => (<Button onPress={() => alert('This is a button!')}
+      title="Info" color="black" />),
+  };
   render() {
+
     return (
-        <Turtle/>
+      <View style={stylesForTodo.container2}>
+        <Text>TodoList Screen</Text>
+        <Text>
+          itemId: {JSON.stringify(this.props.navigation.getParam('itemId', 'NO-ID'))}
+        </Text>
+        <Text>
+          otherParam:{JSON.stringify(this.props.navigation.getParam('otherParam', 'value'))}
+        </Text>
+        <Button
+          title="Go to TodoList... again"
+          onPress={() =>
+            this.props.navigation.push('ToDolist', {
+              itemId: Math.floor(Math.random() * 100),
+            })
+          }
+        />
+      </View>
     );
   }
 }
-
-const TurtleRoute = createStackNavigator(
+const Turtle = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen,
-    
+    ToDolist: TodoList,
+    Calculator: CalculatorClass,
   },
   {
     initialRouteName: 'Home',
   },
-
 );
-
-const AppContainer = createAppContainer(TurtleRoute);
-
-
-export default class NavTurtle extends React.Component {
-  render() {
-    return (
-    <AppContainer />
-      )
-  }
-}
-const TurtleRouteTab = createBottomTabNavigator(
-    {
-      Tab1: HomeScreen,
-      Tab2: DetailsScreen,
-    }
-    
-  );
-  
-  class Tab1Screen extends React.Component {
-    static navigationOptions = {
-      title: 'Hello my home',
-      }; 
-     
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Some Text</Text>
-        </View>
-      );
-    }
-  }
-  
-  
+export const SomeContainer = createAppContainer(Turtle);
